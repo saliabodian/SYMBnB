a37ee68c2412f3ae6caf7256642d8a10c69042bf
Salia Bodian <sbodian@gmail.com>
2019-01-05 01:46:36 +0100

    Mise en place de webpack-encore et gestion des css et de js

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,13 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    //.addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/ad', './assets/js/ad.js')
     .addStyleEntry('css/app', './assets/css/app.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
-    .enableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
